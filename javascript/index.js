e8a025b53b7ecca9b8d5dd5c12fcd2a9f0b863a1
cdavis1809 <c3imaging@outlook.com>
2023-06-25 15:27:51 -0400

    added js function to update real time

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,33 @@
+function updateTime() {
+  //los angeles
+  let losAngelesElement = document.querySelector("#los-angeles");
+  let losAngelesDateElement = losAngelesElement.querySelector(".date");
+  let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+  let losAngelesTime = moment().tz("America/Los_Angeles");
+  losAngelesDateElement.innerHTML = moment().format("MMMM D, YYYY");
+  losAngelesTimeElement.innerHTML = `${losAngelesTime.format(
+    `hh:mm:ss`
+  )} <small>${losAngelesTime.format(`A`)}</small>`;
 
+  //tokyo
+  let tokyoElement = document.querySelector("#tokyo");
+  let tokyoDateElement = tokyoElement.querySelector(".date");
+  let tokyoTimeElement = tokyoElement.querySelector(".time");
+  let tokyoTime = moment().tz("Asia/Tokyo");
+  tokyoDateElement.innerHTML = moment().format("MMMM D, YYYY");
+  tokyoTimeElement.innerHTML = `${tokyoTime.format(
+    `hh:mm:ss`
+  )} <small>${tokyoTime.format(`A`)}</small>`;
+
+  //berlin
+  let berlinElement = document.querySelector("#berlin");
+  let berlinDateElement = berlinElement.querySelector(".date");
+  let berlinTimeElement = berlinElement.querySelector(".time");
+  let berlinTime = moment().tz("Europe/Berlin");
+  berlinDateElement.innerHTML = moment().format("MMMM D, YYYY");
+  berlinTimeElement.innerHTML = `${berlinTime.format(
+    `hh:mm:ss`
+  )} <small>${berlinTime.format(`A`)}</small>`;
+}
+updateTime();
+setInterval(updateTime, 1000);
